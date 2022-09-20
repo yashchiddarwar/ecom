6c1d0879d2abfe6d81dbdcf9f9fa13674d46a683
yashchiddarwar <93822408+yashchiddarwar@users.noreply.github.com>
2022-09-20 23:46:51 +0530

    First Update-05/09/222

--- shop/views.py
+++ shop/views.py
@@ -1,32 +1,45 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import Product
+from math import ceil
 
 # Create your views here.
+from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'shop/index.html')
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
 
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
+    params = {'allProds':allProds}
+    return render(request, 'shop/index.html', params)
 
 def about(request):
-    return HttpResponse("About")
-
+    return render(request, 'shop/about.html')
 
 def contact(request):
-    return HttpResponse("Contact")
-
+    return HttpResponse("We are at contact")
 
 def tracker(request):
-    return HttpResponse("Tracker")
-
+    return HttpResponse("We are at tracker")
 
 def search(request):
-    return HttpResponse("Search")
-
+    return HttpResponse("We are at search")
 
 def productView(request):
-    return HttpResponse("Prod-view")
-
+    return HttpResponse("We are at product view")
 
 def checkout(request):
-    return HttpResponse("Checkout")
+    return HttpResponse("We are at checkout")
